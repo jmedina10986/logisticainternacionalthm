7d519bd116109d4d180fb89b2375949bf351eefe
Jacobo Medina <jmedin@gmail.com>
2021-07-28 13:35:39 -0600

    contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@
 */
 (function() {
   "use strict";
-
+ 
   /**
    * Easy selector helper function
    */
@@ -265,5 +265,35 @@
       mirror: false
     });
   });
+
+
+  $("#sendMail").click(()=> {
+    
+    if ($("#message-contact").val() && $("#name-contact").val() && $("#email-contact").val()) {
+      const message = `name: ${$("#name-contact").val()}<br/>
+      email: ${$("#email-contact").val()}<br/>
+      phone: ${$("#phone-contact").val()}<br/>
+      message: ${$("#message-contact").val()}<br/>`;
+      
+    const data = { 
+      email: $("#email-contact").val(),
+      message: message,
+      subject: $("#subject-contact").val()
+    }
+    
+    console.log(data)
+    axios.post("https://h0gb9bgau4.execute-api.us-east-1.amazonaws.com/production/", data)
+    .then(response => {
+      $(".sent-message").show();
+      $("#message-contact").val('');
+      $("#subject-contact").val('');
+      $("#email-contact").val('');
+      $("#name-contact").val('');
+      $("#phone-contact").val('');
+      setInterval(function(){ location.reload(); }, 4500);
+    })
+    .catch(error => console.log(error));
+  }
+  })
 
 })()
